90f562222b927121a1ffdf613cc230ce485f59ef
phamtuan77 <tuanpv8212@ut.edu.vn>
2026-08-01 10:49:57 +0700

    Update Lab2.py

--- Lab2/Lab2.py
+++ Lab2/Lab2.py
@@ -4,15 +4,15 @@ import numpy as np
 img = cv2.imread("picture.jpg")
 img = cv2.resize(img, (400, 300))
 
-# I.1 Độ sáng
-bright = cv2.convertScaleAbs(img, alpha=1, beta=70)
+# I.1 Cộng điểm ảnh (tăng sáng)
+bright = np.clip(img.astype(int) + 80, 0, 255).astype(np.uint8)
 
-# I.2 Độ tương phản
-contrast = cv2.convertScaleAbs(img, alpha=2, beta=0)
+# I.2 Nhân điểm ảnh (tăng tương phản)
+contrast = np.clip(img.astype(float) * 1.8, 0, 255).astype(np.uint8)
 
-# Ghép ảnh
 result = np.hstack((img, bright, contrast))
-cv2.imshow("Goc | Sang | Tuong phan", result)
+
+cv2.imshow("Original | Brightness | Contrast", result)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
